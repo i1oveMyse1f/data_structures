545f41c7c9684d144443d66eed13d5546fc7f997
i1oveMyse1f <56483480+i1oveMyse1f@users.noreply.github.com>
2019-12-22 12:09:25 +0300

    Update DoubleHash.cpp

--- DoubleHash.cpp
+++ DoubleHash.cpp
@@ -1,97 +1,62 @@
 #include <iostream>
+#include <vector>
 
-template<class T = unsigned>
-struct DoubleHash {
+template<class A = unsigned, class B = int>
+struct HashMap {
 private:
-	T pow(const T a, const T n, const T p) {
-		if (n == 0)
-			return 1;
-		unsigned long long t = pow(a, n >> 1, p);
-		t = t * t % p;
-		if (n & 1)
-			t = t * a % p;
-		return t;
+	inline bool is_prime(int x) {
+		for (int i = 2; i * i <= x; i++)
+			if (x % i == 0)
+				return false;
+		return true;
 	}
-	T inv(const T x, const T p) {
-		return pow(x, p - 2, p);
+
+	inline int find_prime(int l) {
+		while (!is_prime(l))
+			++l;
+		return l;
 	}
+	std::vector<std::pair<A, B>> hash_table; // { key, value }
 
 public:
+	inline HashMap(int size) : hash_table(find_prime(2 * size)) {}
 
-	T x1, x2;
-	static const T MOD1 = 1e9 + 9;
-	static const T MOD2 = 1e9 + 7;
-
-	DoubleHash(T x = 0) : x1(x), x2(x) {}
-	DoubleHash(T x1, T x2) : x1(x1), x2(x2) {}
-
-	DoubleHash operator+(const DoubleHash other) const {
-		T res1 = x1 + other.x1;
-		T res2 = x2 + other.x2;
-
-		if (res1 >= MOD1)
-			res1 -= MOD1;
-		if (res2 >= MOD2)
-			res2 -= MOD2;
-		return { res1, res2 };
+	inline void insert(A x, B value = 1) {
+		auto t = x % hash_table.size();
+		while (hash_table[t].first && hash_table[t].first != x) {
+			++t;
+			if (t == hash_table.size())
+				t = 0;
+		}
+		hash_table[t].first = x;
+		hash_table[t].second += value;
 	}
 
-	DoubleHash operator-(const DoubleHash other) const {
-		T res1, res2;
-		if (x1 >= other.x1)
-			res1 = x1 - other.x1;
-		else
-			res1 = MOD1 + x1 - other.x1;
-
-		if (x2 >= other.x2)
-			res2 = x2 - other.x2;
-		else
-			res2 = MOD2 + x2 - other.x2;
-
-		return { res1, res2 };
+	inline bool contains(A x) const {
+		auto t = x % hash_table.size();
+		while (hash_table[t].first) {
+			if (hash_table[t].first == x)
+				return true;
+			++t;
+			if (t == hash_table.size())
+				t = 0;
+		}
+		return false;
 	}
 
-	DoubleHash operator*(const DoubleHash other) const {
-		return { x1 * 1ULL * other.x1 % MOD1,
-				 x2 * 1ULL * other.x2 % MOD2 };
+	inline B operator[](A x) const {
+		auto t = x % hash_table.size();
+		while (hash_table[t].first) {
+			if (hash_table[t].first == x)
+				return hash_table[t].second;
+			++t;
+			if (t == hash_table.size())
+				t = 0;
+		}
+		return 0;
 	}
 
-	DoubleHash operator+= (const DoubleHash other) {
-		return *this = *this + other;
-	}
-
-	DoubleHash operator-= (const DoubleHash other) {
-		return *this = *this - other;
-	}
-
-	DoubleHash operator*= (const DoubleHash other) {
-		return *this = *this - other;
-	}
-
-	DoubleHash operator/(DoubleHash other) {
-		return { x1 * 1ULL * inv(other.x1, MOD1) % MOD1,
-				 x2 * 1ULL * inv(other.x2, MOD2) % MOD2 };
-	}
-
-	DoubleHash operator/=(const DoubleHash other) {
-		return *this = *this / other;
-	}
-
-	friend std::istream& operator>>(std::istream& in, DoubleHash& a) {
-		in >> a.x1;
-		a.x2 = a.x1;
-		return in;
-	}
-
-	friend std::ostream& operator<<(std::ostream& out, const DoubleHash a) {
-		return out << "{ " << a.x1 << ", " << a.x2 << " } ";
-	}
-
-	bool operator==(const DoubleHash other) const {
-		return x1 == other.x1 && x2 == other.x2;
-	}
-
-	bool operator!=(const DoubleHash other) const {
-		return !(*this == other);
+	inline B count(A x) const {
+		return operator[](x);
 	}
 };
